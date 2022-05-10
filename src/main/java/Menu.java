66ed9d104cd56910d588c395b64295e8107db21c
Cleiton Ribeiro da Silva <cleiton_ribeiro@live.com>
2022-05-10 11:39:43 -0300

    Adicionando a primara parte da história ate o primeiro desafio, tendo que antes da história adicionar a variável que levara a mesma.
    Código comentado em bloco no CASE1.
    
    E por fim adicionando a biblioteca java TimeUnit, para dar
    um tempo entre os comandos acerem executados.

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Menu {
     //Menu do jogo com 3 opções para o jogador.//
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Scanner input = new Scanner(System.in);
         int iniciar;
         do {
@@ -141,7 +142,64 @@ public class Menu {
 
                             System.out.println("\nKevin entra com a senha");
                     }
+                    /*
+                             Primeira parte história 3 ate o primeiro desafio.
 
+                    boolean condicaoResposta = false; int tentativas = 0, erroJogadas=0,acertos=0;
+                    System.out.println("Enquanto Kevin se prepara para jogar, Tyler que perdeu no “perguntados”, diz que vai para a cozinha, \n" +
+                    "nisso Kevin pega o controle e esperar subir vídeo para jogar. \n" +
+                    "Enquanto Tyler vai para a cozinha, no meio do caminho ele houve um barulho estranho" +
+                    " vindo do quarto da sua irmã e vai ver o que está acontecendo \n");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("TYLER: Oooooow, que barulho é esse, vai quebrar a casa é? \n");
+                    //System.out.println("apter enter pra contiar ver a historia...");
+                    //String String = sc.nextLine();
+                    TimeUnit.SECONDS.sleep(4);
+                    System.out.println("JADE: Fica na sua, e não se mete !!!\n\n");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println ("Tyler observa o quarto todo bagunçado, e vê no chão o celular de jade meio escondido no meio de alguns papeis, \n" +
+                            "então sem jade perceber ele pega o celular dela e vê que tem várias \n" +
+                            "chamadas e mensagens de Kevin e acha estranho ");
+
+                    System.out.println("________DESAFIO________\n\n" + //primeiro desafio da história 3
+                            "Tyler movido pela sua curiosidade pega o celular de sua irmã\n" +
+                            "para acessa-lo preisar descobrir a senha. \n" +
+                            "A senha foi criada  com base nos algarismos romanos\n" +
+                            "e na seguinte senquencia \n" +
+                            "Maria\n" +
+                            "jaDe\n" +
+                            "tayLer\n" +
+                            "keVIn\n");
+                    while (!condicaoResposta){
+                        System.out.println("Qual é senha para acessar o celular da Jade:");
+                        int resposta=sc.nextInt();
+                        if (resposta==1556){
+                            System.out.println("celular desbloqueado\n");
+                            condicaoResposta=true;
+                            acertos=acertos+1;
+                        }else {
+                            System.out.println("Senha incorreta\n");
+                            if (tentativas==2){
+                                System.out.println("dica");
+                            }
+                            tentativas+=tentativas;
+                            erroJogadas+=erroJogadas;
+                        }
+                    }
+                    System.out.println("Tyler vê no celular que pela foto de perfil que não é Kevin mandando as mensagens,\n" +
+                            " mas alguém com que Jade já tinha se envolvido no passado e que contribuiu para que ela se envolvesse com coisas erradas");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println("TYLER: Você voltou a se envolver com esse cara ? Você enlouqueceu!!?");
+                    TimeUnit.SECONDS.sleep(4);
+                    System.out.println("JADE : Não é da sua conta o que eu faço da minha vida! ");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(" TYLER: É quando causa problemas para dentro de casa, igual da última vez ");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println("....");
+                    System.out.println ("Enquanto Tyler e Jade discutem, Kevin que estava tentando jogar videogame vê que está demorando demais para o jogo iniciar e decide ir atras de Tyler \n " +
+                            "para avisar que o jogo pode estar com problemas e nisso ele acaba ouvindo a discussão de Tyler e Jade  ");
+
+                    */
                     break;
                 case 2:
                     System.out.println("<<Opa, aqui você encontrar algumas informações sobre os personagens do nosso jogo e também algumas curiosidades!>>"
